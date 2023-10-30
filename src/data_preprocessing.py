ded1c0cdf00c3926af299befd3d8d11d1377c249
luizamfsantos <luiza.marques_@hotmail.com>
2023-10-30 11:08:33 -0300

    feature: function epoch_to_datetime change column TIME_SINCE_EPOCH to TIME where the type is datetime for ease of reading and manipulation

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import sys, os
+from datetime import datetime
 
 def signed_data(data):
     '''
@@ -34,6 +35,41 @@ def signed_data(data):
     data_signed['TRUST_INDEX'] = np.sign(data_signed['TRUST_INDEX'])
     return data_signed
 
+def epoch_to_datetime(data):
+    '''
+    Change the epoch time to datetime
+
+    Parameters
+    ----------
+    data: pandas DataFrame
+        The data to be modified
+        It should have a column called TIME_SINCE_EPOCH
+
+    Returns
+    -------
+    data: pandas DataFrame
+        The modified data
+        Instead of the column TIME_SINCE_EPOCH,
+            there is a column called TIME
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> import numpy as np
+    >>> from datetime import datetime
+    >>> data = pd.DataFrame({'TIME_SINCE_EPOCH': np.array([1546300800, 1546387200, 1546473600, 1546560000])})
+    >>> data_timed = epoch_to_datetime(data)
+    >>> data_timed
+                    TIME
+    0 2019-01-01 00:00:00
+    1 2019-01-02 00:00:00
+    2 2019-01-03 00:00:00
+    3 2019-01-04 00:00:00
+    '''
+    data['TIME'] = data['TIME_SINCE_EPOCH'].apply(lambda x: datetime.fromtimestamp(x))
+    data = data.drop('TIME_SINCE_EPOCH', axis=1)
+    return data
+
 def clean_data(data):
     # Function to clean the raw data
     pass
@@ -50,9 +86,10 @@ def prepare_data(data):
 if __name__ == '__main__':
     # append the path of the project directory to the system path
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-    file_path = "data/raw/soc-sign-bitcoinotc.csv"
-    # lines = 1000
-    # data = pd.read_csv(file_path, nrows=lines,header=0)
-    data = pd.read_csv(file_path, header=0)
-    signed_data = signed_data(data)
-    signed_data.to_csv("data/processed/soc-sign-bitcoinotc-signed.csv", index=False)
+    file_path = "data/processed/soc-sign-bitcoinotc-signed.csv"
+    lines = 100
+    data = pd.read_csv(file_path, nrows=lines,header=0)
+    print(data.head())
+    data_timed = epoch_to_datetime(data)
+    print(data_timed.head())
+    
