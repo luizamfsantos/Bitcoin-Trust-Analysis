b1abd3c5d48043377f7ed17e507ab9fb177457cb
luizamfsantos <luiza.marques_@hotmail.com>
2023-10-31 18:54:41 -0300

    feat: Add function to calculate centrality measures for negative nodes
    This commit adds a new function  that calculates various centrality measures for each node in the . The function computes degree centrality, betweenness centrality, and eigenvector centrality for the nodes in the provided network graph. It also filters the centrality measures to include only the nodes present in the . The function includes a comprehensive docstring and an example demonstrating its usage with the provided  graph.
    
    Changes Made:
    - Added a function to calculate centrality measures for negative nodes
    - Implemented computation of degree centrality, betweenness centrality, and eigenvector centrality
    - Filtered the centralities to include only nodes present in the
    - Added comprehensive docstring and example for the function
    
    The function is expected to enhance the analysis of the provided network with negative TRUST_INDEX values and help in understanding the centrality properties of the nodes in the network.

--- src/network_analysis_utils.py
+++ src/network_analysis_utils.py
@@ -165,3 +165,51 @@ def visualize_network_of_negative_nodes(negative_nodes_graph, negative_node_list
     nx.draw_networkx_edges(negative_nodes_graph, pos, edgelist=negative_nodes_graph.edges(), edge_color='black')
     nx.draw_networkx_nodes(negative_nodes_graph, pos, nodelist=negative_node_list, node_color='r', node_size=500)
     plt.show()
+
+def calculate_centralities_negative_nodes(graph, negative_nodes_list):
+    '''
+    Calculate centrality measures for each node in negative_nodes_list
+        Include calculate degree centrality, betweenness centrality, eigenvector centrality
+    
+    Parameters
+    ----------
+    graph: Weighted DiGraph
+        The network to be analyzed
+    negative_nodes_list: numpy array
+        The nodes that have negative TRUST_INDEX values
+    
+    Returns
+    -------
+    centralities: dict
+        A dictionary containing the centrality measures for each node in negative_nodes_list
+            degree_centrality: degree centrality of each node in negative_nodes_list
+            betweenness_centrality: betweenness centrality of each node in negative_nodes_list
+            eigenvector_centrality: eigenvector centrality of each node in negative_nodes_list
+    
+    Examples
+    --------
+    >>> import networkx as nx
+    >>> import matplotlib.pyplot as plt
+    >>> network_data = nx.DiGraph()
+    >>> network_data.add_edge(1, 2, weight=0.5)
+    >>> network_data.add_edge(1, 3, weight=-9.8)
+    >>> network_data.add_edge(2, 3, weight=-0.5)
+    >>> network_data.add_edge(3, 1, weight=0.5)
+    >>> negative_nodes = np.array([2, 1])
+    >>> centralities = calculate_centralities_negative_nodes(network_data, negative_nodes)
+    >>> centralities['degree_centrality']
+    {1: 0.3333333333333333, 2: 0.3333333333333333}
+    >>> centralities['betweenness_centrality']
+    {1: 0.0, 2: 0.0}
+    >>> centralities['eigenvector_centrality']
+    {1: 0.5773502691896256, 2: 0.5773502691896256}
+    '''
+    centralities = {}
+    centralities['degree_centrality'] = nx.degree_centrality(graph)
+    centralities['betweenness_centrality'] = nx.betweenness_centrality(graph)
+    centralities['eigenvector_centrality'] = nx.eigenvector_centrality(graph)
+    # filter the centralities to only include the nodes in negative_nodes_list
+    negative_nodes_centralities = {}
+    for key in centralities.keys():
+        negative_nodes_centralities[key] = {k: centralities[key][k] for k in negative_nodes_list}
+    return negative_nodes_centralities
